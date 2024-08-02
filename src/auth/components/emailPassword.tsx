3b5d402ee14ddd1b0e4cb28e60aaee35b2be1afa
Abhishek Singh <abhiwebdev2718@gmail.com>
2024-08-02 15:35:51 +0530

    set for TDD

--- src/auth/components/emailPassword.tsx
+++ src/auth/components/emailPassword.tsx
@@ -4,10 +4,7 @@ function EmailPasswordLogin() {
   return (
     <div>
           <h4>Email Password Login</h4>
-          <form>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" />
-          </form>
+         
     </div>
   );
 }
